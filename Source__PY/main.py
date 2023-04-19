e1381dd703321cf10d6f1b912f94128f8bff81af
HaoNguyen <haonp1999@gmail.com>
2023-04-19 17:16:08 +0700

    ok

--- Source__PY/main.py
+++ Source__PY/main.py
@@ -1,1 +1,8 @@
-print("Hello")
+# Sử dụng ba dấu nháy
+print('''He said, "What's there?"''')
+
+# Trường hợp nháy đơn
+print('He said, "What's there?"')
+
+# Trường hợp nháy kép
+print("He said, \"What's there?\"")
